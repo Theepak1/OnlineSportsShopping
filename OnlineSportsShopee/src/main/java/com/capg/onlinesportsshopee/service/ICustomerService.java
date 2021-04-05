3242c16235de39a0e4baf947e1743d1fa2fa002d
Theepak1 <theepakkprakash@gmail.com>
2021-04-05 15:29:00 +0530

    theepak error rectified

--- OnlineSportsShopee/src/main/java/com/capg/onlinesportsshopee/service/ICustomerService.java
+++ OnlineSportsShopee/src/main/java/com/capg/onlinesportsshopee/service/ICustomerService.java
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 package com.capg.onlinesportsshopee.service;
 
 import java.util.List;
-
 import com.capg.onlinesportsshopee.bean.Customer;
 import com.capg.onlinesportsshopee.model.CustomerDTO;
 
@@ -14,20 +12,3 @@ public interface ICustomerService {
 	public CustomerDTO getCustomer(long custId);
 	public List<CustomerDTO> getAllCustomers(); 
 }
-=======
-package com.capg.onlinesportsshopee.service;
-
-import java.util.List;
-
-import com.capg.onlinesportsshopee.bean.Customer;
-import com.capg.onlinesportsshopee.model.CustomerDTO;
-
-
-public interface ICustomerService {
-	public CustomerDTO addCustomer(Customer customer);
-	public CustomerDTO removeCustomer(long custId);
-	public CustomerDTO updateCustomer(Customer customer);
-	public CustomerDTO getCustomer(long custId);
-	public List<CustomerDTO> getAllCustomers(); 
-}
->>>>>>> branch 'master' of https://github.com/Theepak1/OnlineSportsShopping.git
