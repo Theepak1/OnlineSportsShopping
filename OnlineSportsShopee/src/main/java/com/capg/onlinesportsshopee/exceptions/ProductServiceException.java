6bcec6278e990e9c250e2e2129092e48a5faa500
Syed Samsu <Syed Samsu@SYDSAMSU-PC>
2021-04-06 11:20:57 +0530

    Product Commit

---
+++ OnlineSportsShopee/src/main/java/com/capg/onlinesportsshopee/exceptions/ProductServiceException.java
@@ -0,0 +1,13 @@
+package com.capg.onlinesportsshopee.exceptions;
+
+
+public class ProductServiceException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ProductServiceException(String message) {
+		super(message);
+	}
+	
+
+}
