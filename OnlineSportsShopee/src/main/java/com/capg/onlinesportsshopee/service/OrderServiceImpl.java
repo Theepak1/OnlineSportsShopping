56660e7f2d371ad4d6f7d90f41a3761d998f0812
Syed Samsu <Syed Samsu@SYDSAMSU-PC>
2021-04-10 09:59:33 +0530

    project without bug

--- OnlineSportsShopee/src/main/java/com/capg/onlinesportsshopee/service/OrderServiceImpl.java
+++ OnlineSportsShopee/src/main/java/com/capg/onlinesportsshopee/service/OrderServiceImpl.java
@@ -59,7 +59,7 @@ public class OrderServiceImpl implements IOrderService {
 			throw new OrderServiceException("OrderId does not exist to delete");
 		} 
 		else {
-			orderRepo.delete(order.get());
+			orderRepo.deleteById(orderId);
 			if(order.isPresent())
 			{
 				return OrderUtil.convertToOrderDto(order.get());
